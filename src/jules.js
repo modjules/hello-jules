562d6589db300fb7e47a61271634be15a55cdf38
Jules <julianna6622@gmail.com>
2021-10-25 23:10:46 -0500

    changed backround color

--- src/jules.js
+++ src/jules.js
@@ -9,19 +9,30 @@ function App() {
     <div className={myClasses}>
       <div className="main tall flex row">
         <div className="A">
-          <strong>My goals</strong>
+          <strong>Our servises</strong>
           <ul>
-            <li>HTML</li>
-            <li>JavaScript</li>
-            <li>CSS</li>
+            <li>Powder eyebrows</li>
+            <li>Shaded eyliner</li>
+            <li>Lip blush</li>
           </ul>
         </div>
         <div className="B flex grow column">
-         <div className="C">
-         <h1> GO Jules</h1>
+         <div className="C flex row">
+           <div className="Aboutus">
+             <h>About Us</h>
+           </div>
+           <div className="Ourwork">
+             <h>Our work</h>
+           </div>
+           <div className="Testimonials">
+             <h>Testimonials</h>
+             </div> 
+           <div className="Contact">
+             <h>Contact</h>
+           </div>
         </div>
         <div className="H">
-          <p>Any Text</p>
+          <p>Welcome to our studio, where the high-quality Permanent Make-up guaranteed</p>
         </div>
         </div>
       </div>
